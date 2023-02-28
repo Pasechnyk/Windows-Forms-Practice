aa4fe4bb77a92d56a6c9b834cb44631dd262f87f
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-02-28 22:46:32 +0200

    Add Shulte Table Game

---
+++ ShulteTable/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace ShulteTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gameTrackBar = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(286, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shulte Table Game";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(34, 146);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(728, 345);
+            this.panel1.TabIndex = 1;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.LightGray;
+            this.button10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button10.Location = new System.Drawing.Point(509, 242);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(207, 89);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "?";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.LightGray;
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(261, 242);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(207, 89);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "?";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightGray;
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(14, 242);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(207, 89);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "?";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightGray;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(509, 126);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(207, 89);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "?";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightGray;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(261, 126);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(207, 89);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGray;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(14, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(207, 89);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGray;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(509, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(207, 89);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(261, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 89);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(14, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 89);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.RandomBtnClick);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startBtn.Location = new System.Drawing.Point(312, 68);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(150, 54);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.StartBtnClick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(564, 501);
+            this.progressBar.Maximum = 9;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(198, 29);
+            this.progressBar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(440, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Your progress:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(21, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time Spent:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerTick);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.timeTextBox.Location = new System.Drawing.Point(127, 501);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(51, 27);
+            this.timeTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(180, 501);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "seconds.";
+            // 
+            // gameTrackBar
+            // 
+            this.gameTrackBar.Location = new System.Drawing.Point(34, 84);
+            this.gameTrackBar.Maximum = 3;
+            this.gameTrackBar.Name = "gameTrackBar";
+            this.gameTrackBar.Size = new System.Drawing.Size(221, 56);
+            this.gameTrackBar.TabIndex = 8;
+            this.gameTrackBar.ValueChanged += new System.EventHandler(this.GameTrackBarValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Change difficulty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 552);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gameTrackBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ShulteTable";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gameTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar gameTrackBar;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ShulteTable/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Task - Create Shulte table with difficulty meter and time restrictions.
+
+namespace ShulteTable
+{
+    public partial class Form1 : Form
+    {
+        private static Random random = new Random();
+        private int timeInterval;
+        private int secondsPassed;
+        private int index = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // timer method
+        private void GameTimerTick(object sender, EventArgs e)
+        {
+            secondsPassed++;
+            timeInterval--;
+            timeTextBox.Text = secondsPassed.ToString();
+
+            if (timeInterval == 0)
+            {
+                gameTimer.Stop();
+                gameTrackBar.Value = 0;
+                MessageBox.Show($"You lost!\n" +
+                    $"You failed to finish on time.");
+            }
+        }
+
+        // difficulty settings
+        private void ChangeDifficulty(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    timeInterval = 30;
+                    break;
+                case 1:
+                    timeInterval = 20;
+                    break;
+                case 2:
+                    timeInterval = 15;
+                    break;
+                case 3:
+                    timeInterval = 10;
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void GameTrackBarValueChanged(object sender, EventArgs e)
+        {
+            ChangeDifficulty(gameTrackBar.Value);
+        }
+
+        // give random numbers
+        public static void ShuffleNumbers<T>(IList<T> values)
+        {
+            for (int i = values.Count - 1; i > 0; i--)
+            {
+                int k = random.Next(i + 1);
+                T value = values[k];
+                values[k] = values[i];
+                values[i] = value;
+            }
+        }
+
+        // first button click
+        private void StartBtnClick(object sender, EventArgs e)
+        {
+            gameTimer.Start();
+
+            List<int> num = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ShuffleNumbers(num);
+
+            int counter = 0;
+            foreach (Button item in panel1.Controls.OfType<Button>())
+            {
+                item.BackColor = Color.LightGray;
+                item.Text = num[counter].ToString();
+                counter++;
+            }
+        }
+
+        // main game function 
+        private void RandomBtnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            progressBar.Value = index;
+
+            if (index == 9)
+            {
+                gameTimer.Stop();
+                MessageBox.Show($"Congrats!\n Time: {secondsPassed}s.");
+                index = 1;
+            }
+            else if (button.Text == index.ToString())
+            {
+                button.BackColor = Color.AliceBlue;
+                index++;
+            }
+        }
+    }
+}
