f88994a7735871a565f602e344d0ab6a847b9079
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-02-28 22:44:31 +0200

    Add LINQ project

---
+++ LinqW/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace LinqW
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combinedList = new System.Windows.Forms.ListBox();
+            this.sortByAscendingBtn = new System.Windows.Forms.Button();
+            this.avgBtn = new System.Windows.Forms.Button();
+            this.evenValuebtn = new System.Windows.Forms.Button();
+            this.leapYearBtn = new System.Windows.Forms.Button();
+            this.exMarkBtn = new System.Windows.Forms.Button();
+            this.findSymbolBtn = new System.Windows.Forms.Button();
+            this.groupByCountBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboList
+            // 
+            this.combinedList.FormattingEnabled = true;
+            this.combinedList.ItemHeight = 20;
+            this.combinedList.Location = new System.Drawing.Point(368, 37);
+            this.combinedList.Name = "comboList";
+            this.combinedList.Size = new System.Drawing.Size(379, 364);
+            this.combinedList.TabIndex = 0;
+            // 
+            // sortByAscendingBtn
+            // 
+            this.sortByAscendingBtn.Location = new System.Drawing.Point(71, 37);
+            this.sortByAscendingBtn.Name = "sortByAscendingBtn";
+            this.sortByAscendingBtn.Size = new System.Drawing.Size(232, 36);
+            this.sortByAscendingBtn.TabIndex = 1;
+            this.sortByAscendingBtn.Text = "Positive and Ascending";
+            this.sortByAscendingBtn.UseVisualStyleBackColor = true;
+            this.sortByAscendingBtn.Click += new System.EventHandler(this.SortByAscBtnClick);
+            // 
+            // avgBtn
+            // 
+            this.avgBtn.Location = new System.Drawing.Point(71, 88);
+            this.avgBtn.Name = "avgBtn";
+            this.avgBtn.Size = new System.Drawing.Size(232, 36);
+            this.avgBtn.TabIndex = 2;
+            this.avgBtn.Text = "Average of 2-digit Numbers";
+            this.avgBtn.UseVisualStyleBackColor = true;
+            this.avgBtn.Click += new System.EventHandler(this.AverageCountBtnClick);
+            // 
+            // evenValuebtn
+            // 
+            this.evenValuebtn.Location = new System.Drawing.Point(71, 139);
+            this.evenValuebtn.Name = "evenValuebtn";
+            this.evenValuebtn.Size = new System.Drawing.Size(232, 36);
+            this.evenValuebtn.TabIndex = 3;
+            this.evenValuebtn.Text = "Max Even Value";
+            this.evenValuebtn.UseVisualStyleBackColor = true;
+            this.evenValuebtn.Click += new System.EventHandler(this.EvenValueBtnClick);
+            // 
+            // leapYearBtn
+            // 
+            this.leapYearBtn.Location = new System.Drawing.Point(71, 192);
+            this.leapYearBtn.Name = "leapYearBtn";
+            this.leapYearBtn.Size = new System.Drawing.Size(232, 33);
+            this.leapYearBtn.TabIndex = 4;
+            this.leapYearBtn.Text = "Show Leap Years";
+            this.leapYearBtn.UseVisualStyleBackColor = true;
+            this.leapYearBtn.Click += new System.EventHandler(this.LeapYearBtnClick);
+            // 
+            // exMarkBtn
+            // 
+            this.exMarkBtn.Location = new System.Drawing.Point(71, 242);
+            this.exMarkBtn.Name = "exMarkBtn";
+            this.exMarkBtn.Size = new System.Drawing.Size(232, 33);
+            this.exMarkBtn.TabIndex = 5;
+            this.exMarkBtn.Text = "Add Exclamation Mark To String";
+            this.exMarkBtn.UseVisualStyleBackColor = true;
+            this.exMarkBtn.Click += new System.EventHandler(this.ExclMarkBtnClick);
+            // 
+            // findSymbolBtn
+            // 
+            this.findSymbolBtn.Location = new System.Drawing.Point(71, 291);
+            this.findSymbolBtn.Name = "findSymbolBtn";
+            this.findSymbolBtn.Size = new System.Drawing.Size(232, 33);
+            this.findSymbolBtn.TabIndex = 6;
+            this.findSymbolBtn.Text = "Find Symbol in String";
+            this.findSymbolBtn.UseVisualStyleBackColor = true;
+            this.findSymbolBtn.Click += new System.EventHandler(this.FindSymbolBtnClick);
+            // 
+            // groupByCountBtn
+            // 
+            this.groupByCountBtn.Location = new System.Drawing.Point(71, 341);
+            this.groupByCountBtn.Name = "groupByCountBtn";
+            this.groupByCountBtn.Size = new System.Drawing.Size(232, 33);
+            this.groupByCountBtn.TabIndex = 7;
+            this.groupByCountBtn.Text = "Group By Amount of Symbols";
+            this.groupByCountBtn.UseVisualStyleBackColor = true;
+            this.groupByCountBtn.Click += new System.EventHandler(this.GroupByCountBtnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupByCountBtn);
+            this.Controls.Add(this.findSymbolBtn);
+            this.Controls.Add(this.exMarkBtn);
+            this.Controls.Add(this.leapYearBtn);
+            this.Controls.Add(this.evenValuebtn);
+            this.Controls.Add(this.avgBtn);
+            this.Controls.Add(this.sortByAscendingBtn);
+            this.Controls.Add(this.combinedList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox combinedList;
+        private System.Windows.Forms.Button sortByAscendingBtn;
+        private System.Windows.Forms.Button avgBtn;
+        private System.Windows.Forms.Button evenValuebtn;
+        private System.Windows.Forms.Button leapYearBtn;
+        private System.Windows.Forms.Button exMarkBtn;
+        private System.Windows.Forms.Button findSymbolBtn;
+        private System.Windows.Forms.Button groupByCountBtn;
+    }
+}
+
---
+++ LinqW/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Intro to LINQ
+// Task - Make a listBox with 7 functions.
+namespace LinqW
+{
+    public partial class Form1 : Form
+    {
+        List<int> numbers = new List<int>();
+        List<int> years = new List<int>();
+        List<string> strings = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // data lists
+            numbers.AddRange(new[]
+            {
+                 -6, -17, 8, 74, 15, 41, 0, 26, 99, 108, 3
+            });
+            years.AddRange(new[]
+            {
+                2000, 2004, 2007, 2009, 2012, 2016, 2020, 2023
+            });
+            strings.AddRange(new[]
+            {
+                "Black", "cat", "sits", "in", "the", "garden"
+            });
+
+            combinedList.DataSource = numbers;
+        }
+
+        // Task 1 - Sort array by ascending
+        private void SortByAscBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            combinedList.DataSource = numbers.Where(num => num > 0).OrderBy(x => x).ToList();
+        }
+
+        // Task 2 - Find sum of positive 2-digit numbers
+        private void AverageCountBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            var poisitivTwoDigits = numbers.Where(x => x > 0 && x >= 10 && x <= 99).ToList();
+            var average = poisitivTwoDigits.Average();
+
+            MessageBox.Show($"Amount of positive 2-digit values is {poisitivTwoDigits.Count()}" +
+                $" and their value is {average}.");
+        }
+
+        // Task 3 - Show leap years sorted by descending
+        private void LeapYearBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            var leapYears = years.Where(x => DateTime.IsLeapYear(x)).OrderByDescending(x => x).ToList();
+
+            combinedList.DataSource = leapYears;
+        }
+
+        // Task 4 - Find max even number 
+        private void EvenValueBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            var evenNumbers = numbers.Where(num => num % 2 == 0).ToList();
+            var maxEven = evenNumbers.Max();
+
+            combinedList.Items.Add(maxEven);
+        }
+
+        // Task 5 - Add 3 exclamation marks to string
+        private void ExclMarkBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            var updatedString = strings.Select(x => x + "!!!").ToList();
+
+            combinedList.DataSource = updatedString;
+        }
+
+        // Task 6 - Show string with given symbol
+        private void FindSymbolBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            char symbol = 'c';
+            var symbolString = strings.Where(x => x.Contains(symbol)).ToList();
+
+            combinedList.DataSource = symbolString;
+        }
+
+        // Task 7 - Group strings by symbols amount
+        private void GroupByCountBtnClick(object sender, EventArgs e)
+        {
+            combinedList.DataSource = null;
+            var groupedString = strings.GroupBy(x => x.Length);
+
+            foreach (var item in groupedString)
+            {
+                combinedList.Items.Add(item.Key);
+            }
+        }
+    }
+}
