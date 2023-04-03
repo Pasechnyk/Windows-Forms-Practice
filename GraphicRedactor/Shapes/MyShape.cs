2f306786f65478e9683e422684cea55fd6f8271d
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-04-03 15:23:30 +0300

    Add Graphic editor (Paint) project

---
+++ GraphicRedactor/Shapes/MyShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicRedactor.Shapes
+{
+    public interface IShape
+    {
+        void Print(Graphics graphics);
+    }
+}
